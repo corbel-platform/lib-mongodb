19a0f3d820b02d1d3c48575a2eeed24359bc390e
Alberto J. Rubio <yebenes9@gmail.com>
2016-07-27 11:25:06 +0200

    Refactor deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 cd `dirname $0`/..
 
-if [ -z "$SONATYPE_USERNAME" ]
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]] && [[ "${TRAVIS_BRANCH}" = "master" ]]
 then
-    echo "error: please set SONATYPE_USERNAME and SONATYPE_PASSWORD environment variable"
-    exit 1
-fi
+    if [ -z "$SONATYPE_USERNAME" ]
+    then
+        echo "Error: Please set SONATYPE_USERNAME and SONATYPE_PASSWORD environment variable"
+        exit 1
+    fi
 
-if [ -z "$SONATYPE_PASSWORD" ]
-then
-    echo "error: please set SONATYPE_PASSWORD environment variable"
-    exit 1
-fi
+    if [ -z "$SONATYPE_PASSWORD" ]
+    then
+        echo "Error: Please set SONATYPE_PASSWORD environment variable"
+        exit 1
+    fi
 
-if [ ! -z "$TRAVIS_TAG" ]
-then
-    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    if [ ! -z "$TRAVIS_TAG" ]
+    then
+        echo "On a tag -> Set pom.xml <version> to $TRAVIS_TAG"
+        mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    else
+        echo "Not on a tag -> Keep snapshot version in pom.xml"
+    fi
+
+    mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
 else
-    echo "not on a tag -> keep snapshot version in pom.xml"
+    echo "Not deploying dependant builds for ${TRANVIS_BRANCH}"
 fi
-
-mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
